097d75966c4a8b20f07e03b692cf87521b1240e8
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-01-13 19:46:48 +0900

    debug

--- main.ts
+++ main.ts
@@ -5,8 +5,10 @@
  */
 export async function once({defaultRegion,kv}:{defaultRegion?:string,kv?:Deno.Kv}) {
   const dr = defaultRegion ||Deno.env.get("DENO_REGION")
+  console.log("deRegion:",dr)
   kv = kv || await Deno.openKv()
   const region = await kv.get(["deno-deploy-once","region"])
+  console.log("region:",region)
   if (region) { 
     if (region.value === dr) {
       console.log("Already ran in this region")
